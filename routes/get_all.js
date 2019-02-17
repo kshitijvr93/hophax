4e80511b264808763987a88439f898389e98c452
Kshitij Ranganath <kshitijvr93@gmail.com>
2019-02-17 07:23:28 -0500

    changed json format

--- routes/get_all.js
+++ routes/get_all.js
@@ -4,35 +4,36 @@ var con = val.con;
 var router = express.Router();
 var http = require("http");
 var https = require("https");
-var twilio = require('twilio');
-var accountSid = ''; // Your Account SID from www.twilio.com/console
-var authToken = '';   // Your Auth Token from www.twilio.com/console
+// var twilio = require('twilio');
+// var accountSid = 'AC7af586e321fa7f7456680ff33334685e'; // Your Account SID from www.twilio.com/console
+// var authToken = 'e01a699246f453380eac63afa37a1667';   // Your Auth Token from www.twilio.com/console
 
 
-var client = new twilio(accountSid, authToken);
+// var client = new twilio(accountSid, authToken);
 
 
 
 
 router.get("/", function(req, res) {
 
-  client.messages.create({
-    body: 'Hello from Node',
-    to: '+1',  // Text this number
-    from: '+1' // From a valid Twilio number
-})
-.then((message) => console.log(message.sid));
+//   client.messages.create({
+//     body: 'Hello from Node',
+//     to: '+16093391767',  // Text this number
+//     from: '+1' // From a valid Twilio number
+// })
+// .then((message) => console.log(message.sid));
 
     
-    var response = { };
+    var response = [];
     var num = 50;
     con.query("SELECT * FROM recipe limit "+num, function (err, result) {
     if (err) throw err;
     
     result.forEach(function(element) {
-    response[element.recipe_name]  = {
-      id: element.id 
-    };
+    dict={ };
+    dict["recipe_id"] = element.id;
+    dict["recipe_name"]  = element.recipe_name;
+    response.push(dict);
     
     });
     res.json(response);
